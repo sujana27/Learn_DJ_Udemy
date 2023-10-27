08ea561f6597d95ebe2d9caa0adea8312153ebc5
Sujana Saha <sujanasaha27@gmail.com>
2023-10-27 16:22:56 -0400

    Created django project and app

--- profile_project/settings.py
+++ profile_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "rest_framewok.authtoken",
+    "rest_framework.authtoken",
     "profiles_api",
 ]
 
